e6e859c154817ba0b1535a79c916b148bbe978f7
Cyrille <c.derche@me.com>
2021-03-30 17:23:04 +0200

    Fix PaginationAlt

--- src/components/Pagination/PaginationAlt.js
+++ src/components/Pagination/PaginationAlt.js
@@ -12,11 +12,11 @@ export default function PaginationAlt({
   return (
     <>
       <PaginationButton pageIndex={pageIndex} gotoPage={gotoPage} pageNumber={0} />
-      { (pageIndex - 2) > 0 ? <Button variant='white' disabled><MoreHoriz size={16} /></Button> : <></> }
+      <Button variant='white' disabled><MoreHoriz size={16} /></Button>
       <PaginationButton pageIndex={pageIndex} gotoPage={gotoPage} pageNumber={pageIndex - 1} />
       <PaginationButton pageIndex={pageIndex} gotoPage={gotoPage} pageNumber={pageIndex} />
       <PaginationButton pageIndex={pageIndex} gotoPage={gotoPage} pageNumber={pageIndex + 1} />
-      { (pageIndex + 2) < (pageCount - 1) ? <Button variant='white' disabled><MoreHoriz size={16} /></Button> : <></> }
+      <Button variant='white' disabled><MoreHoriz size={16} /></Button>
       <PaginationButton pageIndex={pageIndex} gotoPage={gotoPage} pageNumber={pageCount - 1} />
     </>
   )
